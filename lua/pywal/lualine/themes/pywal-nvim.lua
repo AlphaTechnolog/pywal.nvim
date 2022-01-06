2839d252068c456f547e12b8eff0aa709106df14
Gabriel <gfranklings@gmail.com>
2022-01-06 17:34:25 -0400

    lualine: some colors refactors

--- lua/pywal/lualine/themes/pywal-nvim.lua
+++ lua/pywal/lualine/themes/pywal-nvim.lua
@@ -1,32 +1,32 @@
-local pywal = {}
 local core = require('pywal.core')
-
 local colors = core.get_colors()
 
+local pywal = {}
+
 pywal.normal = {
-  a = { bg = colors.color7, fg = colors.background },
-  b = { bg = colors.background, fg = colors.foreground },
+  a = { bg = colors.color4, fg = colors.background },
+  b = { bg = colors.background, fg = colors.color7 },
   c = { bg = colors.background, fg = colors.foreground },
 }
 
 pywal.insert = {
-  a = { bg = colors.color4, fg = colors.background },
-  b = { bg = colors.color4, fg = colors.background },
+  a = { bg = colors.color2, fg = colors.background },
+  b = { bg = colors.background, fg = colors.color4 },
 }
 
 pywal.command = {
   a = { bg = colors.color5, fg = colors.background },
-  b = { bg = colors.color5, fg = colors.background },
+  b = { bg = colors.background, fg = colors.color5 },
 }
 
 pywal.visual = {
   a = { bg = colors.color6, fg = colors.background },
-  b = { bg = colors.foreground, fg = colors.color6 },
+  b = { bg = colors.background, fg = colors.color6 },
 }
 
 pywal.replace = {
   a = { bg = colors.color11, fg = colors.background },
-  b = { bg = colors.foreground, fg = colors.color11 },
+  b = { bg = colors.background, fg = colors.color11 },
 }
 
 pywal.inactive = {
