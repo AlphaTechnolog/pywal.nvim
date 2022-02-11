d67f68b283c98d5a9a81657f09182f0438780bc8
gabriel <gfranklings@gmail.com>
2022-02-10 20:56:55 -0400

    fixed comments issues

--- lua/pywal/config.lua
+++ lua/pywal/config.lua
@@ -29,7 +29,7 @@ M.highlights_base = function (colors)
     CursorColumn = { guibg = colors.background },
     ColorColumn = { guibg = colors.background },
     NormalFloat = { guibg = colors.background },
-    Visual = { guibg = colors.color1 },
+    Visual = { guibg = colors.color1, guifg = colors.foreground },
     VisualNOS = { guibg = colors.background },
     WarningMsg = { guifg = colors.color3, guibg = colors.background },
     DiffAdd = { guifg = colors.background, guibg = colors.color4 },
@@ -115,6 +115,7 @@ M.highlights_base = function (colors)
     -- TSCharacter         = { };    -- For characters.
     -- TSComment           = { };    -- For color1 blocks.
     TSNote = { guifg = colors.background, guibg = colors.color5 },
+    TSComment = { guifg = colors.color1 },
     TSWarning = { guifg = colors.background, guibg = colors.color5 },
     TSDanger = { guifg = colors.background, guibg = colors.color3 },
     TSConstructor = { guifg = colors.color6 }, -- For constructor calls and definitions: `= { }` in Lua, and Java constructors.
